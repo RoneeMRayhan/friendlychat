c29c63ead08c8b75188c8c1e5806a7684c50892f
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2019-07-02 08:57:38 +0600

    Send Button colored with Theme Accent Color...

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(new FriendlychatApp());
 }
 
@@ -12,7 +12,6 @@ class FriendlychatApp extends StatelessWidget {
       home: new ChatScreen(),
     );
   }
-
 }
 
 class ChatScreen extends StatefulWidget {
@@ -25,31 +24,35 @@ class ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("FriendlyChat")),
-
       body: _buildTextComposer(),
     );
   }
+
   final TextEditingController _textController = new TextEditingController();
-  Widget _buildTextComposer(){
-    return new Container(
-      margin: new EdgeInsets.symmetric(horizontal: 8.0),
-      child: new Row(
-        children: <Widget>[
-          new Flexible(
-            child: TextField(
-              controller: _textController,
-              onSubmitted: _handleSubmitted,
-              decoration: new InputDecoration.collapsed(
-                  hintText: "Send a message"),
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: new EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
             ),
-          ),
-          new Container(
-            margin: new EdgeInsets.symmetric(horizontal: 4.0),
-            child: new IconButton(
-                icon: new Icon(Icons.send),
-                onPressed: () => _handleSubmitted(_textController.text)),
-          ),
-        ],
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
       ),
     );
   }
