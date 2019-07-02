aa37087e8599758f7aff9989dbb967ffce73d83e
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2019-07-02 07:20:51 +0600

    Split into two classes...

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    new MaterialApp(
-      title: "FriendlyChat",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("friendly_chat"),
-        ),
-      ),
-    ),
-  );
+  runApp(new FriendlychatApp());
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "friendly_chat",
+      home: new ChatScreen(),
+    );
+  }
+
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("FriendlyChat")),
+    );
+  }
 }
